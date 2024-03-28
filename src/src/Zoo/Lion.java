0702f0d2ce923ef07d5519c4662b391ab001e829
Rollinnolan <109935701+Rollinnolan@users.noreply.github.com>
2024-03-27 20:29:32 -0700

    Add files via upload

---
+++ src/src/Zoo/Lion.java
@@ -0,0 +1,35 @@
+package Zoo;
+
+public class Lion extends Animal
+{
+    //Constructor
+    public Lion(String name){
+        super(name);
+        this.name= name;
+        this.species = "lion";
+    }
+    public Lion(String name, int age)
+    {
+        super(name,age);
+        this.name = name;
+        this.age = age;
+        this.species = "lion";
+    }
+    public Lion(String name, int age, String season, String color, String sex, String weight, String location, String habitat)
+    {
+        super(name, age, season, color, sex, weight, location, habitat);
+        this.name = name;
+        this.age = age;
+        this.season = season;
+        this.color = color;
+        this.sex = sex;
+        this.weight = weight;
+        this.location = location;
+        this.habitat = habitat;
+        this.species = "lion";
+        numOfAnimals--;
+    }
+    public void sing(){
+        System.out.println("It's the circle of life.");
+    }
+}
